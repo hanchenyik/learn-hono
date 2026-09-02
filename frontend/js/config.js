00069f4e1c61502ccffdef78ba76f2ede0eb4a20
han <hanchenyik520@gmail.com>
2026-09-02 22:32:09 +0800

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_BASE: location.hostname === 'localhost' ? 'http://localhost:8787' : 'https://petitbakery-api.velozz.workers.dev'
+  API_BASE: location.hostname === 'localhost' ? '' : 'https://petitbakery-api.velozz.workers.dev'
 }
